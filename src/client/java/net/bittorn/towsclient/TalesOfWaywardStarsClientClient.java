7a056d35c8c82ffd99c773f544278c320ea18678
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-07-19 15:09:58 +1000

    Template init

---
+++ src/client/java/net/bittorn/towsclient/TalesOfWaywardStarsClientClient.java
@@ -0,0 +1,10 @@
+package net.bittorn.towsclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TalesOfWaywardStarsClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
