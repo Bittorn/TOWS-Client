28e1e39dbeefac2e892a6b6d0db045ca2b2f8002
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-07-19 19:47:13 +1000

    Resource loading basics

--- src/client/java/net/bittorn/towsclient/TOWSClient.java
+++ src/client/java/net/bittorn/towsclient/TOWSClient.java
@@ -3,12 +3,19 @@ package net.bittorn.towsclient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
+
+import java.io.InputStream;
 
 public class TOWSClient implements ClientModInitializer {
 
@@ -16,12 +23,35 @@ public class TOWSClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// Keybinds
 		KeyBinding interactBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.towsclient.interact", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "key.category.towsclient"));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (interactBinding.wasPressed()) {
 				client.player.sendMessage(Text.literal("Interact was pressed!"), false); // placeholder logic
+			}
+		});
+
+		// Resources
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+			@Override
+			public Identifier getFabricId() {
+				return Identifier.of("towsclient", "resources");
+			}
+
+			@Override
+			public void reload(ResourceManager manager) {
+				// Clear caches here
+
+				for (Identifier id : manager.findResources("resource_folder", path -> path.toString().endsWith(".json")).keySet()) {
+					try (InputStream stream = manager.getResourceOrThrow(id).getInputStream()) {
+						// Consume the stream
+						// Remember to close resource stream after processing.
+						// Forgetting to do this is an easy way to end up with a resource leak.
+					} catch (Exception e) {
+						logErrorLoading("Error occurred while loading resource JSON " + id.toString());
+					}
+				}
 			}
 		});
 	}
