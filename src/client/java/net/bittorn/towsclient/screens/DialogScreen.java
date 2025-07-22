5163b1a7d54e1b71530d30e269e0c7dd4ed15385
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-07-22 21:51:36 +1000

    Basic dialog screen

--- src/client/java/net/bittorn/towsclient/screens/DialogScreen.java
+++ src/client/java/net/bittorn/towsclient/screens/DialogScreen.java
@@ -1,13 +1,25 @@
 package net.bittorn.towsclient.screens;
 
+import net.bittorn.towsclient.TOWSClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.awt.*;
 
 public class DialogScreen extends Screen {
+    public static final Identifier DIALOG_BOX_TEXTURE = Identifier.of(TOWSClient.MOD_ID, "textures/gui/dialog/dialog_box_gui.png");
 
     public DialogScreen(Text title) {
         super(title);
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false; // cannot pause on server
     }
 
     @Override
@@ -22,11 +34,27 @@ public class DialogScreen extends Screen {
 
     @Override
     protected void init() {
-        // do some stuff
+        // do stuff here
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
+
+        assert this.client != null;
+
+        int imageWidth = 512;
+        int imageHeight = 128;
+
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) - 24;
+
+        String dialog = "This is some placeholder dialog!";
+
+        context.drawTexture(RenderLayer::getGuiTextured, DIALOG_BOX_TEXTURE, x, y, 0, 0, imageWidth, imageHeight, imageWidth, imageHeight, new Color(255, 255, 255, 127).hashCode());
+
+        context.drawWrappedTextWithShadow(this.textRenderer, StringVisitable.plain(dialog),
+                width / 2 - this.textRenderer.getWidth(dialog) / 2, y + imageHeight / 2,
+                imageWidth - 24, 0xFFFFFFFF);
     }
 }
