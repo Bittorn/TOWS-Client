b9c489b3dc3cb2acf2f43845dcc1db2b0888a94e
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-07-29 12:25:40 +1000

    Updated mixin

--- src/client/java/net/bittorn/towsclient/mixin/client/ClientJoinMixin.java
+++ src/client/java/net/bittorn/towsclient/mixin/client/ClientJoinMixin.java
@@ -1,6 +1,7 @@
 package net.bittorn.towsclient.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,12 +9,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public class ClientLoadMixin {
-	@Inject(at = @At("HEAD"), method = "onFinishedLoading")
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientJoinMixin {
+	@Inject(at = @At("RETURN"), method = "onGameJoin")
 	private void init(CallbackInfo info) {
 		MinecraftClient.getInstance().getToastManager().add(
-				SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.NARRATOR_TOGGLE, Text.of("TOWS Client loaded successfully"), Text.empty())
+				SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.NARRATOR_TOGGLE, Text.of("TOWS Client loaded"), Text.empty())
 		);
 	}
 }
