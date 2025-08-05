c7ee3ea180712df2ed98c884eb38b5ce4cd5b770
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-08-05 17:43:25 +1000

    Update StoryManager.java

--- src/client/java/net/bittorn/towsclient/data/StoryManager.java
+++ src/client/java/net/bittorn/towsclient/data/StoryManager.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 
 public class StoryManager {
     public Story loadStory(String filename) {
-        InputStream systemResourceAsStream = ClassLoader.getSystemResourceAsStream(filename);
+        InputStream systemResourceAsStream = ClassLoader.getSystemResourceAsStream(filename); // not work !!
 
         // try-with-resources !!
         try (BufferedReader br = new BufferedReader(
