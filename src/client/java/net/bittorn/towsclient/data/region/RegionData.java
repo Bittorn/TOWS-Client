3de2c618b6b5f1d6bfeb513e60255375ebbfc2d7
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-15 00:46:47 +1000

    no idea whats going on here tbh

---
+++ src/client/java/net/bittorn/towsclient/data/region/RegionData.java
@@ -0,0 +1,11 @@
+package net.bittorn.towsclient.data.region;
+
+import java.util.ArrayList;
+
+public class RegionData {
+    private static String name;
+    private static int startX;
+    private static int startZ;
+    private static int endX;
+    private static int endZ;
+}
---
+++ src/client/java/net/bittorn/towsclient/data/region/RegionsData.java
@@ -0,0 +1,7 @@
+package net.bittorn.towsclient.data.region;
+
+import java.util.ArrayList;
+
+public class RegionsData {
+    private static ArrayList<RegionData> regions;
+}
