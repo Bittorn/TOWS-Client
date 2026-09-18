7a484def075119acd58c05ade6ee38025cc3e12c
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-09-18 17:33:52 +0930

    Create ConfigScreen.kt

---
+++ src/main/kotlin/com/windtempos/tows/screens/ConfigScreen.kt
@@ -0,0 +1,7 @@
+package com.windtempos.tows.screens
+
+import net.minecraft.client.gui.screens.Screen
+import net.minecraft.network.chat.Component
+
+class ConfigScreen(component: Component) : Screen(component) {
+}
