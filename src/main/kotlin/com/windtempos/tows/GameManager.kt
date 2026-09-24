b0fa1ba78b4ab0987ba14a473ae835a86c614ff8
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-09-24 16:13:02 +1000

    switch gamemanager type to object

--- src/main/kotlin/com/windtempos/tows/GameManager.kt
+++ src/main/kotlin/com/windtempos/tows/GameManager.kt
@@ -1,16 +1,5 @@
 package com.windtempos.tows
 
-class GameManager private constructor() {
-    companion object {
-
-        @Volatile // ensure instance property is updated atomically
-        private var instance: GameManager? = null
-
-        fun getInstance() =
-            instance ?: synchronized(this) { // prevent accessing method from multiple threads simultaneously
-                instance ?: GameManager().also { instance = it }
-            }
-    }
-
+object GameManager {
     fun doSomething() = "Doing something"
 }
